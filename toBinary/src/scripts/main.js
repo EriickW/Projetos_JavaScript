f1686da6d7246e5b6c3f53dbc20273bda89742e1
EriickW <werick631@gmail.com>
2022-08-07 18:23:44 -0300

    .

--- toBinary/src/scripts/main.js
+++ toBinary/src/scripts/main.js
@@ -1,7 +1,15 @@
 const input = document.querySelector("input")
 const botao = document.querySelector("button")
-const numeroConvertido = document.querySelector("numeroConvertido")
+const numeroConvertido = document.querySelector("#numeroConvertido")
 
 botao.addEventListener("click",()=>{
-    
+    let numero = input.value
+
+    let binary = (numero%2).toString();
+
+    while(numero > 1){
+        numero = parseInt(numero /2)
+        binary = (numero % 2) + (binary)
+    }
+    numeroConvertido.innerText = binary
 })
