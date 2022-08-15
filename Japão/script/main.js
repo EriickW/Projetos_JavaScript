8baaf263d8c937dafa40aae3dd8ddb0bf3f2f072
EriickW <werick631@gmail.com>
2022-08-15 15:03:21 -0300

    Termino do projeto Japão

--- Japão/script/main.js
+++ Japão/script/main.js
@@ -1,7 +1,12 @@
 const menu = document.querySelector("#menu")
 const body = document.querySelector("body")
-
-
+const avancar = document.querySelector("#avançar")
+const voltar = document.querySelector("#voltar")
+const castelo = document.querySelector("#castelo")
+const cidade1 = document.querySelector("#cidade1")
+const cidade2 = document.querySelector("#cidade2")
+const circle = document.getElementsByClassName("circle")
+//========= menu =====================
 menu.addEventListener("click", ()=>{
     body.classList.toggle("menu-expanded")
     if(menu.getAttribute("name") == "Menu"){
@@ -12,5 +17,51 @@ menu.addEventListener("click", ()=>{
         menu.setAttribute("name","Menu")
     }
 })
+//======================================
 
-console.log()
+//========= navigation ===========
+avancar.addEventListener("click", ()=>{
+
+     if(castelo.classList == "secContent active"){
+        console.log(true)
+            castelo.classList.remove("active")
+            cidade1.classList.add("active")
+            circle[0].classList.remove("active")
+            circle[1].classList.add("active")
+        } else if(cidade1.classList == "secContent active"){
+            console.log(true)
+                cidade1.classList.remove("active")
+                cidade2.classList.add("active")
+                circle[1].classList.remove("active")
+            circle[2].classList.add("active")
+            }else if(cidade2.classList == "secContent active"){
+                console.log(true)
+                    cidade2.classList.remove("active")
+                    castelo.classList.add("active")
+                    circle[2].classList.remove("active")
+                    circle[0].classList.add("active")
+            } 
+})
+voltar.addEventListener("click", ()=>{
+
+    if(castelo.classList == "secContent active"){
+            console.log(true)
+            castelo .classList.remove("active")
+            cidade2.classList.add("active")
+            circle[0].classList.remove("active")
+            circle[2].classList.add("active")
+       }else if(cidade2.classList == "secContent active"){
+           console.log(true)
+               cidade2.classList.remove("active")
+               cidade1.classList.add("active")
+               circle[2].classList.remove("active")
+               circle[1].classList.add("active")
+           }else if(cidade1.classList == "secContent active"){
+               console.log(true)
+               cidade1.classList.remove("active")
+               castelo.classList.add("active")
+               circle[1].classList.remove("active")
+               circle[0].classList.add("active")
+
+           } 
+})
