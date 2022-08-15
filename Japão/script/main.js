a99e709e25efa5a1499c0e4706ce57c3fccdafb0
EriickW <werick631@gmail.com>
2022-08-14 22:01:12 -0300

    .

--- Japão/script/main.js
+++ Japão/script/main.js
@@ -4,4 +4,13 @@ const body = document.querySelector("body")
 
 menu.addEventListener("click", ()=>{
     body.classList.toggle("menu-expanded")
+    if(menu.getAttribute("name") == "Menu"){
+        menu.removeAttribute("name")
+        menu.setAttribute("name","close-outline")
+    }else{
+        menu.removeAttribute("name")
+        menu.setAttribute("name","Menu")
+    }
 })
+
+console.log()
